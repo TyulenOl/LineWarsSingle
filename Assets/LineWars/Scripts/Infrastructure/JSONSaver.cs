873ad5e6756646715857d1dfebeb2fc4e73e46ab
BestGydak <115108761+BestGydak@users.noreply.github.com>
2023-12-22 12:16:20 +0500

    JSONSaver

---
+++ Assets/LineWars/Scripts/Infrastructure/JSONSaver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace LineWars.Model
+{
+    public class JSONSaver<TValue, TOut> : ISaver<TValue>
+    {
+        private readonly IConverter<TValue, TOut> converter;
+        private readonly Func<string, int, string> pathGenerator;
+        public JSONSaver(IConverter<TValue, TOut> converter, Func<string, int, string> pathGenerator) 
+        { 
+            this.converter = converter;
+            this.pathGenerator = pathGenerator;
+        }
+
+        public void Save(TValue value, int id)
+        {
+            var newDeckInfo = converter.Convert(value);
+            var json = JsonUtility.ToJson(newDeckInfo);
+            var objectName = nameof(TValue);
+            var path = pathGenerator(objectName, id);
+            File.WriteAllText(path, json);
+        }
+    }
+}
