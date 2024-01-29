b58145431d7559dc857f44a1fdeb36030638e890
BestGydak <115108761+BestGydak@users.noreply.github.com>
2024-01-29 19:34:56 +0500

    Fighting Spirit Effect

--- Assets/LineWars/Scripts/Model/Units/effects/FightingSpiritEffect.cs
+++ Assets/LineWars/Scripts/Model/Units/effects/FightingSpiritEffect.cs
@@ -1,9 +1,5 @@
-﻿using LineWars.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace LineWars.Model
 {
@@ -12,21 +8,121 @@ namespace LineWars.Model
         where TEdge : class, IEdgeForGame<TNode, TEdge, TUnit>
         where TUnit : class, IUnit<TNode, TEdge, TUnit>
     {
-        private int powerBonus;
         public override EffectType EffectType => EffectType.FightingSpirit;
-        public FightingSpiritEffect(TUnit targetUnit) : base(targetUnit)
+
+        public FightingSpiritEffect(TUnit targetUnit, int powerBonus) : base(targetUnit)
         {
+            this.powerBonus = powerBonus;
         }
 
+        private int powerBonus;
+        private HashSet<TUnit> collectedUnits;
+        private HashSet<TNode> subscribedNodes;
 
         public override void ExecuteOnEnter()
         {
-            throw new NotImplementedException();
+            TargetUnit.UnitNodeChanged += OnUnitNodeChanged;
+            CollectAllUnits();
+            SubscribeNodes();
         }
 
         public override void ExecuteOnExit()
         {
-            throw new NotImplementedException();
+            TargetUnit.UnitNodeChanged -= OnUnitNodeChanged;
+            ClearSubscribedNodes();
+            DeleteAllUnits();
+        }
+
+        private void CollectUnit(TUnit unit)
+        {
+            if(collectedUnits.Contains(unit))
+            {
+                Debug.LogError("Unit already collected");
+                return;
+            }
+            collectedUnits.Add(unit);
+            TargetUnit.CurrentPower += powerBonus;
+        }
+
+        private void DeleteUnit(TUnit unit)
+        {
+            if (!collectedUnits.Contains(unit))
+            {
+                Debug.LogError("Can't collect not collected unit!");
+                return;
+            }
+            collectedUnits.Remove(unit);
+            TargetUnit.CurrentPower -= powerBonus;
+        }
+
+        private void RecollectUnits()
+        {
+            ClearSubscribedNodes();
+            DeleteAllUnits();
+            SubscribeNodes();
+            CollectAllUnits();
+        }
+
+        private void ClearSubscribedNodes()
+        {
+            foreach (var node in subscribedNodes)
+            {
+                node.UnitLeft -= OnUnitLeft;
+                node.UnitAdded -= OnUnitAdded;
+            }
+
+            subscribedNodes.Clear();
+        }
+
+        private void DeleteAllUnits()
+        {
+            var units = new List<TUnit>(collectedUnits);
+            foreach (var unit in units)
+            {
+                DeleteUnit(unit);
+            }
+        }
+
+        private void CollectAllUnits()
+        {
+            var neighbors = TargetUnit.Node.GetNeighbors();
+            foreach (var neighbor in neighbors)
+            {
+                if (neighbor.LeftUnit != null)
+                {
+                    CollectUnit(neighbor.LeftUnit);
+                }
+                if (neighbor.RightUnit != null && neighbor.LeftUnit != neighbor.RightUnit)
+                {
+                    CollectUnit(neighbor.RightUnit);
+                }
+            }
+        }
+
+        private void SubscribeNodes()
+        {
+            var neighbors = TargetUnit.Node.GetNeighbors();
+            foreach (var neighbor in neighbors)
+            {
+                neighbor.UnitLeft += OnUnitLeft;
+                neighbor.UnitAdded += OnUnitAdded;
+                subscribedNodes.Add(neighbor);
+            }
+        }
+
+        private void OnUnitNodeChanged(TUnit unit, TNode _, TNode _1)
+        {
+            RecollectUnits();
+        }
+
+        private void OnUnitLeft(TNode node, TUnit unit)
+        {
+            DeleteUnit(unit);
+        }
+
+        private void OnUnitAdded(TNode node, TUnit unit)
+        {
+            CollectUnit(unit);
         }
     }
 }
